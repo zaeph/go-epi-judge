86decc97bb6012aeca5d8aa91b781cbc3ddd4ad8
Leo Vivier <zaeph@zaeph.net>
2022-06-25 08:32:13 +0200

    parity: Add other less-optimized solutions

--- epi/parity/solution.go
+++ epi/parity/solution.go
@@ -1,12 +1,34 @@
 package parity
 
+// func Parity(x int64) int16 {
+// 	// Bruteforce
+// 	var p int64
+// 	p = 0
+// 	for x > 0 {
+// 		p ^= x & 0x1
+// 		x >>= 1
+// 	}
+// 	return int16(p)
+// }
+
+// func Parity(x int64) int16 {
+// 	// Optimized
+// 	var p int16
+// 	p = 0
+// 	for x > 0 {
+// 		p ^= 1
+// 		x = x & (x - 1)
+// 	}
+// 	return p
+// }
+
 func Parity(x int64) int16 {
+	// Best
 	x ^= x >> 32
 	x ^= x >> 16
 	x ^= x >> 8
 	x ^= x >> 4
 	x ^= x >> 2
 	x ^= x >> 1
-	p := int16(x & 0x1)
-	return p
+	return int16(x & 0x1)
 }
