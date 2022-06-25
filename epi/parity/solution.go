e23533429a60e3ffcddd63f4941776f602bab3bf
Leo Vivier <zaeph@zaeph.net>
2022-06-25 08:16:39 +0200

    parity: Add solution

--- epi/parity/solution.go
+++ epi/parity/solution.go
@@ -1,6 +1,12 @@
 package parity
 
 func Parity(x int64) int16 {
-	// TODO - Add your code here
-	return 0
+	x ^= x >> 32
+	x ^= x >> 16
+	x ^= x >> 8
+	x ^= x >> 4
+	x ^= x >> 2
+	x ^= x >> 1
+	p := int16(x & 0x1)
+	return p
 }
